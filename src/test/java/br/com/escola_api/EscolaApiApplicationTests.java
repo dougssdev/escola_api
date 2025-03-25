6b6fa1d9654bf79158fa7f30268361c48fb0bbf4
dougssdev <douglassabinosilva@hotmail.com>
2025-03-25 09:51:55 -0300

    Commit inicial

---
+++ src/test/java/br/com/escola_api/EscolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escola_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
